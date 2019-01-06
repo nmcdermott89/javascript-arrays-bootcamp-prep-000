4557b1aa05bd758f895f866c394ee9a96c1fd8d8
Nicholas McDermott <nmcdermott89@gmail.com>
2019-01-06 04:56:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [ 'snicker', 'hundred grand', 'kitkat', 'skittles' ]
+
+function addElementToBeginningOfArray(chocolateBars, "milkyway") {
+
+  chocolateBars.unshift("milkyway")
+  
+
+}
+
+
+function destructivelyAddElementToBeginningOfArray()
